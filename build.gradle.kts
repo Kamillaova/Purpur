3af753cddae56bee86424e2e125b7fa585411fa4
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-11-19 23:03:09 +0300

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,42 @@
 plugins {
-    `java-library`
-    `maven-publish`
-    id("xyz.jpenilla.toothpick")
+  `java-library`
+  `maven-publish`
+  id("xyz.jpenilla.toothpick")
 }
 
 toothpick {
-    forkName = "Purpur"
-    groupId = "net.pl3x.purpur"
-    forkUrl = "https://github.com/pl3xgaming/Purpur"
-    val versionTag = System.getenv("BUILD_NUMBER")
-        ?: "\"${commitHash() ?: error("Could not obtain git hash")}\""
-    forkVersion = "git-$forkName-$versionTag"
+  forkName = "Purpur"
+  groupId = "dev.kamillaova.purpur"
+  forkUrl = "https://github.com/Kamillaova/Purpur"
+  val versionTag = System.getenv("BUILD_NUMBER")
+    ?: "\"${commitHash() ?: error("Could not obtain git hash")}\""
+  forkVersion = "git-$forkName-$versionTag"
 
-    minecraftVersion = "1.16.5"
-    nmsPackage = "1_16_R3"
-    nmsRevision = "R0.1-SNAPSHOT"
+  minecraftVersion = "1.16.5"
+  nmsPackage = "1_16_R3"
+  nmsRevision = "R0.1-SNAPSHOT"
 
-    upstream = "Paper"
-    upstreamBranch = "origin/master"
+  upstream = "Paper"
+  upstreamBranch = "ver/1.16.5"
 
-    server {
-        project = projects.purpurServer.dependencyProject
-        patchesDir = rootProject.projectDir.resolve("patches/server")
-    }
-    api {
-        project = projects.purpurApi.dependencyProject
-        patchesDir = rootProject.projectDir.resolve("patches/api")
-    }
+  server {
+    project = projects.purpurServer.dependencyProject
+    patchesDir = rootProject.projectDir.resolve("patches/server")
+  }
+  api {
+    project = projects.purpurApi.dependencyProject
+    patchesDir = rootProject.projectDir.resolve("patches/api")
+  }
 }
 
 subprojects {
-    repositories {
-        maven("https://nexus.velocitypowered.com/repository/velocity-artifacts-snapshots/")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/") {
-            name = "sonatype-oss-snapshots"
-        }
-    }
+  repositories {
+    maven("https://nexus.velocitypowered.com/repository/velocity-artifacts-snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+  }
 
-    java {
-        sourceCompatibility = JavaVersion.toVersion(17)
-        targetCompatibility = JavaVersion.toVersion(17)
-    }
-
-    publishing.repositories.maven {
-        url = uri("https://repo.pl3x.net/snapshots")
-        credentials(PasswordCredentials::class)
-    }
+  java {
+    sourceCompatibility = JavaVersion.toVersion(17)
+    targetCompatibility = JavaVersion.toVersion(17)
+  }
 }
