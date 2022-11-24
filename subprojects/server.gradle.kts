6a76feeebc83cc4df9d5729b97fa6c0c4daacd03
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-11-24 20:40:36 +0300

    fix shitty wtf mistake

--- subprojects/server.gradle.kts
+++ subprojects/server.gradle.kts
@@ -1,3 +1,9 @@
 dependencies {
     implementation("cat.inspiracio", "rhino-js-engine", "1.7.10")
 }
+
+configurations.configureEach {
+    resolutionStrategy {
+        force("org.slf4j:slf4j-api:1.7.36")
+    }
+}
